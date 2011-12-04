cb7b6f5cda75072019398a9cddb80bdeb89c40f5
Kenan Shifflett <kenan.shifflett@gmail.com>
2011-12-04 00:59:34 -0500

    tested register against old endpoint

---
+++ examples/register.js
@@ -0,0 +1,23 @@
+var dwolla = require('../lib/dwolla');
+var c = require('../config');
+
+var newUser = {};
+newUser.email = '';
+newUser.password = '';
+newUser.pin = '';
+newUser.firstName = '';
+newUser.lastName = '';
+newUser.address = '';
+newUser.address2 = '';
+newUser.city = '';
+newUser.state = '';
+newUser.zip = '';
+newUser.phone = '';
+newUser.dob = '';
+newUser.acceptTerms = 'true';
+
+dwolla.register(c.client_id, c.client_secret, newUser, function(err, user) {
+  console.log(err);
+  console.log(user);
+});
+
